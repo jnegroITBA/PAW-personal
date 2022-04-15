636a42c8414f70339eafdba871508f2a0701a1c5
Juan Negro <jnegro@itba.edu.ar>
2022-04-15 14:02:48 -0300

    Fix findByUsername Commit

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/UserDao.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/UserDao.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 public interface UserDao {
     Optional<User> getUserById(long id);
+    Optional<User> findByUsername(String username);
 
     User create(String username, String password);
 
